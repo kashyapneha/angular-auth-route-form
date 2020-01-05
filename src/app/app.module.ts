34582caa2113fea0fd8e39d484e3ff79d6f9aeaa
kashyapneha <kashyap_neha@network.lilly.com>
2020-01-06 00:33:59 +0530

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes, Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { Form1Component } from './form1/form1.component';
@@ -12,6 +13,7 @@ import { Form2Component } from './form2/form2.component';
 
 const appRoutes: Routes = [
     { path:'', component: HomeComponent },
+    { path:'auth', component: AuthComponent },
     { path:'form1', component: Form1Component },
     { path:'form2', component: Form2Component}
   ];
@@ -23,6 +25,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes) ],
   declarations: [ 
     AppComponent, 
+    AuthComponent,
     HeaderComponent,
     HomeComponent,
     Form1Component,
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 
+import { AuthComponent } from '../auth/auth.component';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  isAuthenticated=true;
 
+  constructor(private auth: AuthComponent){}
+
+  onSubmit() {
+    this.isAuthenticated = true;
+  }
 }
