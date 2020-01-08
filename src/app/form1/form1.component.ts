b7c810b8ba50e0eec70c8430b0b1a0d0ea0110b0
kashyapneha <kashyap_neha@network.lilly.com>
2020-01-08 10:29:45 +0530

    4 files modified, 1 added

--- src/app/form1/form1.component.ts
+++ src/app/form1/form1.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form1',
-  templateUrl: './form1.component.html'
+  templateUrl: './form1.component.html',
+  styleUrls: ['./form1.component.css']
 })
 export class Form1Component {
+  langs: string[] = ['English', 'German', 'French'];
 
+  isLoading = false;
+
+  @ViewChild('f',{static:false}) submitForm : NgForm;
+
+  onFormSubmit(){
+    this.isLoading = true;
+    console.log('form submitted!', this.submitForm.value);
+    this.submitForm.reset();
+    this.isLoading = false;
+  }
 }
